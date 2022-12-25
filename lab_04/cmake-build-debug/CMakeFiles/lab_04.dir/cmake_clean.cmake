210e0f7838bc7f9adff681e022b9f79b31c074ea
mokiva <rikusha02@yandex.ru>
2022-12-25 14:08:07 +0300

    local changes

---
+++ lab_04/cmake-build-debug/CMakeFiles/lab_04.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab_04.dir/main.c.o"
+  "lab_04"
+  "lab_04.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab_04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
